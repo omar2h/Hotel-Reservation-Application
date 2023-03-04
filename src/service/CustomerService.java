f0630102f9b72a48ecd91262cf4ca1623c2b598a
omarsamak <omarhesham83@gmail.com>
2023-03-04 12:48:52 +0200

    add logic to CustomerService

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -2,13 +2,17 @@ package service;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 import model.Customer;
 
 public class CustomerService {
+    private Map<String, Customer> customersData;
     private static CustomerService customerServiceInstance;
 
-    private CustomerService(){}
+    private CustomerService(){
+        customersData = new HashMap<>();
+    }
 
     public static CustomerService getCustomerService(){
         if(customerServiceInstance == null){
@@ -17,15 +21,21 @@ public class CustomerService {
         return customerServiceInstance;
     }
 
-    public void addCustomer(String email, String firstName, String lastName){
-        ;
+    public void addCustomer(String email, String firstName, String lastName) throws IllegalArgumentException {
+        if(customersData.containsKey(email))
+            throw new IllegalArgumentException("Email already exists");
+        
+        customersData.put(email, new Customer(firstName, lastName, email));
     }
 
-    public Customer getCustomer(String customerEmail){
-        return new Customer("dummy", "dummy", "dummy");
+    public Customer getCustomer(String customerEmail) throws IllegalArgumentException {
+        if(!customersData.containsKey(customerEmail))
+            throw new IllegalArgumentException("Email does not exist");
+
+        return customersData.get(customerEmail);
     }
 
     public Collection<Customer> getAllCustomers(){
-        return (Collection<Customer>) new HashMap();
+        return customersData.values();
     }
 }
